46bcf0971bc89a930ff596b0ac974d375b9503ad
RoderickLabel <roderickruotolo@gmail.com>
2019-09-09 19:33:04 -0300

    Organizing files

--- js/main.js
+++ js/main.js
@@ -1011,46 +1011,6 @@ var BaseShelter = function (x, y) {
 
 BaseShelter.prototype = Object.create(ObjectGame.prototype);
 
-var SoundsManager = {
-    sounds : {
-        shoot : "shoot.wav",
-        explosion : "explosion.wav",
-        ufoLowpitch : "ufo_lowpitch.wav",
-        ufoHighpitch : "ufo_highpitch.wav",
-        fastInvader1 : "fastinvader1.wav",
-        fastInvader2 : "fastinvader2.wav",
-        fastInvader3 : "fastinvader3.wav",
-        fastInvader4 : "fastinvader4.wav",
-        invaderKilled : "invaderkilled.wav"
-    },
-    loadSounds : function () {
-        for (soundId in this.sounds) {
-            createjs.Sound.registerSound('sounds/' + this.sounds[soundId], soundId);
-        }
-    },
-    playSound: function (soundId) {
-        createjs.Sound.play(soundId);
-    },
-    backgroundSounds: [
-        'fastInvader4',
-        'fastInvader1',
-        'fastInvader2',
-        'fastInvader3'
-    ],
-    currentSoundTrack : 0,
-    nexSoundTrack : function () {
-        if (this.currentSoundTrack >= this.backgroundSounds.length - 1) {
-            this.currentSoundTrack = 0; 
-        } else {
-            this.currentSoundTrack++;
-        }
-    },
-    playSoundTrack : function () {
-        console.log(this.backgroundSounds[this.currentSoundTrack]);
-        this.playSound(this.backgroundSounds[this.currentSoundTrack]);
-        this.nexSoundTrack();
-    }
-};
 
 var SetUp = function () {
     this.init = function () {        
